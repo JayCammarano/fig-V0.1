79808577e2ce85876f277826f397a4869750ee4e
JayCammarano <jay.cammarano@gmail.com>
2020-07-31 23:07:28 -0400

    generated label, artist, release, and tag models

---
+++ db/migrate/20200801025347_create_releases.rb
@@ -0,0 +1,14 @@
+class CreateReleases < ActiveRecord::Migration[5.2]
+  def change
+    create_table :releases do |t|
+      t.string :release_type, null: false
+      t.string :embed_url
+      t.text :title, null: false
+      t.integer :original_release_year, null: false
+      t.belongs_to :artist, null: false
+      t.belongs_to :label, null: false
+
+      t.timestamps
+    end
+  end
+end
