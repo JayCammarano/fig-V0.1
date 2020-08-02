d67d066755519091480bbf6981905f435d9f94ae
JayCammarano <jay.cammarano@gmail.com>
2020-08-02 18:05:45 -0400

    updated releasetile for link

--- app/javascript/react/components/releases/ReleaseTile.js
+++ app/javascript/react/components/releases/ReleaseTile.js
@@ -7,22 +7,21 @@ const ReleaseTile = ({
   original_release_year,
   release_type,
   release_id,
-  label_id,
 }) => {
   return (
     <div className="column is-one-third m-t-lg">
       <div className="card has-background-light">
-
         <p className="has-text-weight-bold has-text-grey m-sm">
           <b>{release_type}</b>
         </p>
         <figure className="image is-48by48 m-sm">
           <img src="" className="card-image" alt="Cover Image" />
         </figure>
-        <Link to={`/artist/${artist_id}/${release}`}>
-        <h4 className="card-header-title has-text-dark">
-          {title} - {original_release_year}
-        </h4></Link>
+        <Link to={`/artist/${artist_id}/releases/${release_id}`}>
+          <h4 className="card-header-title has-text-dark">
+            {title} - {original_release_year}
+          </h4>
+        </Link>
         <p className="pl-4 has-text-dark">Short Description</p>
         <p className="center">Associated Label - Cat Number</p>
         <p className="center">Associated Tags</p>
