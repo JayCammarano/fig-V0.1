fd24b6f6c99b062fdd1043d2a637b2dabb1caf66
JayCammarano <jay.cammarano@gmail.com>
2020-08-02 16:24:11 -0400

    added artists_releases join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_203757) do
+ActiveRecord::Schema.define(version: 2020_08_02_202223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 2020_08_01_203757) do
     t.text "alias", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "artists_releases", id: false, force: :cascade do |t|
+    t.bigint "artist_id", null: false
+    t.bigint "release_id", null: false
   end
 
   create_table "labels", force: :cascade do |t|
