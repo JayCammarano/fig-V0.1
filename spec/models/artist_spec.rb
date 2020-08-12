e017cb5e051d33d0b31a231de6804162ad39d57d
JayCammarano <jay.cammarano@gmail.com>
2020-08-12 11:19:10 -0400

    added artist model tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "releaseImageCaller" do
+    let!(:release1) {FactoryBot.create(:release)}
+
+    context "when an artist has a release" do
+      it "gets related releases" do
+        artist1 = release1.artists.first
+        releases = artist1.releaseImageCaller
+      
+        expect(releases[0][:id]).to eq(artist1.releases.first.id)
+        expect(releases[0][:title]).to eq(artist1.releases.first.title)
+      end
+    end
+  end
+  describe "lastfmCaller" do
+    let!(:release1) {FactoryBot.create(:release)}
+
+    context "when an artist has a release" do
+      it "gets related releases" do
+        artist1 = release1.artists.first
+        lastFMInfo = artist1.lastfmCaller
+
+        expect(lastFMInfo[:bio]).not_to be_empty
+      end
+    end
+  end
 end
